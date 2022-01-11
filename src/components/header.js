062c84aec715c166711fff28fcf31a22ea636f65
Ahihiii <94583574+Ahihiii@users.noreply.github.com>
2022-01-11 21:14:07 +0700

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import Nav from "./nav";
+
+const Header = {
+    render() {
+        return /* html */`
+        <header class="max-w-5xl mx-auto">
+            <div class="bg-blue-800 py-4">
+                <img src="../../images/logo.png" class="mx-auto w-40" />
+            </div>
+            <div class="bg-orange-500">
+            ${Nav.render()}
+            </div>
+        </header>`;
+    },
+};
+export default Header;
